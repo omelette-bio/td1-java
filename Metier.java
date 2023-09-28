2cafd9ec3f5fcce32b0c00ddf38a638c5f5bb4b3
omelette-bio <omelettebio@proton.me>
2023-09-28 12:52:48 +0200

    modified:   Metier.java

--- Metier.java
+++ Metier.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+
 public class Metier {
     protected String nom;
     protected String description;
-    protected List<Sort> tab;
-    
+    protected ArrayList<Sort> sorts = new ArrayList<>();
+
+    public static final String magicien = "magicien";
+    public static final String magicienDescription = "Le magicien est un personnage qui utilise la magie pour se battre.";
+    public static final String guerrier = "guerrier";
+    public static final String guerrierDescription = "Le guerrier est un personnage qui utilise la force pour se battre.";
+    public static final String barbare = "barbare";
+    public static final String barbareDescription = "Le barbare est un personnage qui utilise la force pour se battre.";
+
+    public Metier(String nom) {
+        this.nom = nom;
+        // if (this.nom == magicien){}
+    }
 }
