4b2c445ee2c1b7cb1891765c16c09a2e633afd2d
omelette <omelettebio@proton.me>
2023-10-04 14:14:55 +0200

    modified:   PersonnageJoueur.java modified:   uml.txt

--- PersonnageJoueur.java
+++ PersonnageJoueur.java
@@ -1,9 +1,24 @@
 public class PersonnageJoueur extends Personnage{
     protected Metier metierJoueur;
-    protected String equipementJoueur;
+    protected Stuff equipementJoueur;
     
     PersonnageJoueur(String nomJ, int metierJ){
         super(nomJ);
         this.metierJoueur = new Metier(metierJ);
+        // make a switch on the metierJ to set the equipementJoueur
+        switch(this.metierJoueur.nom){
+            case Metier.magicien:
+                this.equipementJoueur = new Stuff("Dague","Une dague en fer", 2);
+                
+                break;
+            case Metier.guerrier:
+                this.equipementJoueur = new Stuff("Epée","Une épée en fer", 4);
+                break;
+            case Metier.barbare:
+                this.equipementJoueur = new Stuff("Hache", "Une hache en fer", 5);
+                break;
+            default:
+                break;
+        }
     }
 }
