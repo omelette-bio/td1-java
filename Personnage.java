210d5156df8e816cda7f56cda180afc23bd903d3
JokerOnWeed <61050822+va704801@users.noreply.github.com>
2023-10-05 11:36:22 +0200

    Update Personnage.java

--- Personnage.java
+++ Personnage.java
@@ -1,20 +1,16 @@
 public class Personnage{
     protected String nom;
-    protected int niveau;
     protected int pvMax;
     protected int pv;
-    protected int manMax;
-    protected int mana;
 
     Personnage(String nomPersonnage){
         this.nom = nomPersonnage;
-        this.niveau = 1;
-        this.pvMax = 1;
-        this.pv = 1;
-        this.manMax = 1;
-        this.mana = 1;
+        this.pvMax = 5;
+        this.pv = 5;
     }
-
-    /* public void attaquer(int degatsAttaque){}
-    public void recevoirDegat(int degatsSubis){} */
+    public void recevoirDegat(int degatsSubis){}
+    
+    public String afficherNom(){
+        return this.nom;
+    }
 }
