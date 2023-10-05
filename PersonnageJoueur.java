dc62745ce53640d13b93da86812968ffb6c677e6
JokerOnWeed <61050822+va704801@users.noreply.github.com>
2023-10-05 11:36:27 +0200

    Update PersonnageJoueur.java

--- PersonnageJoueur.java
+++ PersonnageJoueur.java
@@ -1,22 +1,25 @@
-public class PersonnageJoueur extends Personnage{
+public class PersonnageJoueur extends PersonnageActif{
     protected Metier metierJoueur;
     protected Stuff equipementJoueur;
+    protected Lieu lieuJoueur;
     
     PersonnageJoueur(String nomJ, int metierJ){
         super(nomJ);
-        this.metierJoueur = new Metier(metierJ);
-        switch(this.metierJoueur.nom){
-            case Metier.magicien:
-                this.equipementJoueur = new Stuff("Dague","Une dague en fer", 2);
+        switch (metierJ) {
+            case 1:
+                this.metierJoueur = new Magicien();
                 break;
-            case Metier.guerrier:
-                this.equipementJoueur = new Stuff("Epée","Une épée en fer", 4);
-                break;
-            case Metier.barbare:
-                this.equipementJoueur = new Stuff("Hache", "Une hache en fer", 5);
+            case 2:
+                this.metierJoueur = new Guerrier();
                 break;
             default:
+                this.metierJoueur = new Barbare();
                 break;
         }
+        this.pvMax = metierJoueur.pvMax;
+        this.pv = metierJoueur.pv;
+        this.paMax = metierJoueur.paMax;
+        this.pa = metierJoueur.pa;
+        this.equipementJoueur = metierJoueur.stuff;
     }
 }
