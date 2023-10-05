50cd8fa3f616d05eaf6a14fa3aa1863d1e562f32
JokerOnWeed <61050822+va704801@users.noreply.github.com>
2023-10-05 11:36:19 +0200

    Update Monstre.java

--- Monstre.java
+++ Monstre.java
@@ -1,11 +1,5 @@
-public class Monstre extends Personnage{
-    int degatsMonstre;
-    Monstre(String nomMonstre, int degatsM, int pvM, int manaM){
+public class Monstre extends PersonnageActif{
+    Monstre(String nomMonstre){
         super(nomMonstre);
-        this.degatsMonstre = degatsM;
-        this.pvMax = pvM;
-        this.pv = pvM;
-        this.manMax = manaM;
-        this.mana = manaM;
     }
 }
