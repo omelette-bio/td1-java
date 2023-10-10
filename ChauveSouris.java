5ca8eaa67ce2fc9fef841d3111960eb5b858fb48
Carla Boinon <103248051+caarlab@users.noreply.github.com>
2023-10-10 22:02:34 +0200

    Create ChauveSouris.java

---
+++ ChauveSouris.java
@@ -0,0 +1,11 @@
+public class ChauveSouris extends Monstre {
+  public static final String nom = "ChauveSouris";
+
+  ChauveSouris() {
+    super (nom);
+    this.pvMax = 6;
+    this.pv = 6;
+
+    this.degats = 5;    
+  }
+}
