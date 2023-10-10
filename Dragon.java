39381c535311a63429e65070d831e3e45570673d
Carla Boinon <103248051+caarlab@users.noreply.github.com>
2023-10-10 22:03:08 +0200

    Create Dragon.java

---
+++ Dragon.java
@@ -0,0 +1,16 @@
+public class Dragon extends Monstre {
+  public static final String nom = "Dragon";
+
+  Dragon() {
+    super (nom);
+    this.pvMax = 70;
+    this.pv = 70;
+
+    this.degats = 12;    
+
+    this.paMax = ;
+    this.pa = ;
+
+    
+  }
+}
