287b0a763936824f22b8af721c66bc1161e155a3
Carla Boinon <103248051+caarlab@users.noreply.github.com>
2023-10-10 22:02:54 +0200

    Create Necromant.java

---
+++ Necromant.java
@@ -0,0 +1,15 @@
+public class Necromant extends Monstre {
+  public static final String nom = "Necromant";
+
+  Necromant() {
+    super (nom);
+    this.pvMax = 30;
+    this.pv = 30;
+
+    this.degats = 8;
+
+    this.paMax = ;
+    this.pa = ;
+
+  }
+}
