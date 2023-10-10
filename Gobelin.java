dd5e2bfd995c86ccf1a49f2c6826d2b19b7ff6ba
Carla Boinon <103248051+caarlab@users.noreply.github.com>
2023-10-10 22:01:21 +0200

    Create Gobelin.java

---
+++ Gobelin.java
@@ -0,0 +1,11 @@
+public class Gobelin extends Monstre {
+  public static final String nom = "Gobelin";
+
+  Gobelin() {
+    super (nom);
+    this.pvMax = 4;
+    this.pv = 4;
+
+    this.degats = 4;
+  }
+}
